c6597b48b51c9304658817fa924bc30d9431bb04
KWayne <summerblog9704@gmail.com>
2022-12-21 15:50:19 +0800

    fix insert_question by Amanda and new index.php

--- SQL_Query/insert_question.sql
+++ SQL_Query/insert_question.sql
@@ -1,15 +1,16 @@
 INSERT INTO questions(text_content, created_at, updated_at, difficulty)
 VALUES("XX input_text_content xx", "XX real_createdtime xx", "XX real_updatedtime xx", "xx input_difficulty xx");
+--question id and other data from php--
+INSERT INTO question_chapter(question_id, chapter_id, created_at, updated_at)
+VALUES("xx input_question_id xx", "xx input_chapter xx", "xx real_createdtime xx", "xx real_updatedtime xx");
 
-INSERT INTO question_chapter(chapter_id, created_at, updated_at)
-VALUES("xx input_chapter xx", "xx real_createdtime xx", "xx real_updatedtime xx");
+INSERT INTO question_textbook(question_id, textbook_id, "page", created_at, updated_at)
+VALUES("xx input_question_id xx", "xx input_textbook_id xx", "XX input_page XX", "xx real_createdtime xx", "xx real_updatedtime xx");
 
-INSERT INTO question_textbook(textbook_id, "page", created_at, updated_at)
-VALUES("xx input_textbook_id xx", "XX input_page XX", "xx real_createdtime xx", "xx real_updatedtime xx");
+INSERT INTO question_skill(question_id, skill_id, created_at, updated_at)
+VALUES("xx input_question_id xx", "xx input_skill_id xx", "xx real_createdtime xx", "xx real_updatedtime xx");
 
-INSERT INTO question_skill(skill_id, created_at, updated_at)
-VALUES("xx input_skill_id xx", "xx real_createdtime xx", "xx real_updatedtime xx");
-
+/*
 CREATE VIEW addingid AS
 SELECT MAX(questions.id) AS now_questions_id, 
        MAX(question_chapter.id) AS now_question_chapter_id,
@@ -31,3 +32,4 @@ UPDATE question_skill
     SET question_id = now_questions_id
     FROM question_skill, addingid
     WHERE question_skill.id = addingid.now_question_skill_id;
+*/
