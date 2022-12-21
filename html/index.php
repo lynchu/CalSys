6f98150ff31b14b48a574eaef3cf6800af6994fe
KWayne <summerblog9704@gmail.com>
2022-12-21 22:20:31 +0800

    fix the web entry

--- html/index.php
+++ html/index.php
@@ -1,57 +1,21 @@
-<?php
-
-$host= 'database-1.cvot66pyfjis.us-east-1.rds.amazonaws.com';
-$db = 'final_project';
-$user = 'postgres';
-$password = 'OAjVnynUXrrPgJ0'; // change to your password
-
-try {
-	$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
-	// make a database connection
-	$conn = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-	if ($conn) {
-		echo "Connected to the $db database successfully!";
-	}
-} catch (PDOException $e) {
-	die($e->getMessage());
-}
-
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>tex_content</th></tr>";
-
-class TableRows extends RecursiveIteratorIterator {
-  function __construct($it) {
-    parent::__construct($it, self::LEAVES_ONLY);
-  }
-
-  function current() {
-    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-  }
-
-  function beginChildren() {
-    echo "<tr>";
-  }
-
-  function endChildren() {
-    echo "</tr>" . "\n";
-  }
-}
-
-try {
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("SELECT id, tex_content FROM original_data");
-  $stmt->execute();
-
-  // set the resulting array to associative
-  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    echo $v;
-  }
-} catch(PDOException $e) {
-  echo "Error: " . $e->getMessage();
-}
-$conn = null;
-echo "</table>";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="text/x-mathjax-config">
+	MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+    </script>
+    <script type="text/javascript"
+        src="http://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+    </script>
+</head>
+<body>
+<p>
+  <?php include 'sqlexe.php'?>
+</p>
+    
+</body>
+</html>
