70215d8dec950fc9fafbda72f455c8d5a8188a77
KWayne <summerblog9704@gmail.com>
2022-12-28 02:35:00 +0800

    fix navigation bar on mobile

--- html/navbar.php
+++ html/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light" style="background-color: #1C1C1C">
-  <div class="container-fluid">
+  <div class="container-fluid navbar-collapse collapse ">
     <a href="./index.php" class="navbar-brand" style="color: #D9D9D9;">
       <img src="https://cdn-icons-png.flaticon.com/512/1774/1774106.png" width="50" height="50" alt="">
     </a>
