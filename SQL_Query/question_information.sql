a1088c876af9d744434527aed9e15b041b41dfe9
lynchu <104618407+lynchu@users.noreply.github.com>
2023-01-04 14:55:43 +0800

    delete technique(skills) attribute

--- SQL_Query/question_information.sql
+++ SQL_Query/question_information.sql
@@ -1,8 +1,7 @@
 SELECT questions.id, textbook_id, book_name, isbn_10, isbn_13, chapter, chapter_name, 'page',
-    text_content, difficulty, skill_id, questions.created_at, questions.updated_at
-FROM textbooks, questions, question_chapter, chapters, question_texbook, skills 
+    text_content, difficulty, questions.created_at, questions.updated_at
+FROM textbooks, questions, question_chapter, chapters, question_texbook
 WHERE chapters.chapter = question_chapter.chapter 
     AND textbooks_id = question_textbook.texbook_id
     AND question_chapter.question_id = question_textbook.question_id
-    AND questions.skill_id = skills.skill_id
     AND questions.id = question_chapter.question_id;
