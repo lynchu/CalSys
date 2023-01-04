78bae29182b6159e6c15a0f9e7994b17dd68d451
lynchu <104618407+lynchu@users.noreply.github.com>
2023-01-04 14:58:06 +0800

    delete technique attribute when deleting question

--- SQL_Query/delete_question.sql
+++ SQL_Query/delete_question.sql
@@ -7,5 +7,7 @@ WHERE question_chapter.question_id = "xx input_question_id xx"
 delete from question_textbook
 where question_textbook.question_id = "xx input_question_id xx"
 
+/*
 delete from question_skill
 WHERE question_skill.question_id = "xx input_question_id xx"
+*/
