97faceb40a904908b5fb2e8f2f788f20f45fb63c
KWayne <summerblog9704@gmail.com>
2023-01-05 21:58:45 +0800

    fix little text

--- html/index.php
+++ html/index.php
@@ -13,7 +13,7 @@
             <p class="lead mb-4">
                 微積分題庫系統是一個幫助學生練習和測驗微積分知識的工具。
                 它包含豐富的題庫，涵蓋了微積分的各個方面，包括微分學、積分學和解析幾何等。
-                通過使用這個系統，學生可以有效地提高自己的微積分技能，並在考試中取得更好的成績。 by chatGPT
+                通過使用這個系統，學生可以有效地提高自己的微積分技能，並在考試中取得更好的成績。 - chatGPT
             </p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                 <a href="./chapter_list.php"  type="button" class="btn btn-primary btn-lg px-4 gap-3">Chapter List</a>
